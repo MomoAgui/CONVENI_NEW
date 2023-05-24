58887ebc03d8d9c79a33ef8c10fc98556456d3a8
MomoAgui <00516440momo@gmail.com>
2023-05-24 14:18:59 +0000

    新規

---
+++ resources/views/famimaice.blade.php
@@ -0,0 +1,50 @@
+@extends('layout')
+
+{{-- メインコンテンツ --}}
+@section('contets')
+        <img src="/img/.png">
+        <h1>ファミリーマートで購入できる主食</h1><br>
+
+        <h2>焼きおむすび</h2><br>
+        <img src="/img/famima/yakiomusubi.png" width="300" height="200"><br>
+
+        <h2>焼しゃけ</h2><br>
+        <img src="/img/famima/yakisyake.png" width="300" height="200"><br>
+
+
+
+
+        <h2>銀鮭の塩焼き</h2><br>
+        <img src="/img/famima/sake.png" width="300" height="200"><br>
+        <p>原材料：銀鮭（チリ産）、食塩、砂糖、米酢、魚醤、食用植物油脂、（一部にさけ、大豆を含む）</p>
+        <p>アレルギー：さけ、大豆</p>
+        <p>内容量：1切れ60g</p>
+        <p>食塩相当量：0.7g /切</p>
+        <p>目安量：9か月ごろ～1歳ごろは約1/4切れ程度</p>
+        <p>1~3歳ごろは約1/2~2/3切れ程度</p>
+
+
+        <h2>さばの塩焼き</h2><br>
+        <img src="/img"><br>
+        <p></p>
+
+        <h2>7種具材の豚汁</h2><br>
+        <img src="/img/famima/tonnziru.png"　width="300" height="200"><br>
+        <p></p>
+
+
+        <h2></h2><br>
+        <img src="/img"><br>
+        <p></p>
+
+
+
+        <h2></h2><br>
+        <img src="/img"><br>
+        <p></p>
+
+
+
+
+
+@endsection
